d3ad709ab0eafa896d36cab45d45408278b22103
OmerFnarco <omer.amasib@fnrco.com.sa>
2022-09-29 15:06:00 +0300

    add new templates

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
+from .models import Book
+from django.http import Http404
 # Create your views here.
 
 def index(request):
-    return render(request, 'book_outlet/index.html')
+    books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        'books': books
+        })
+
+def book_detail(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+        return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestselling': book.is_bestselling
+        })
+    except Book.DoesNotExist:
+        raise Http404('Book does not exist')
+    
