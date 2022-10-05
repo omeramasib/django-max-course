f580f2b0cd65944bc693e753978ad66c1b121c7f
OmerFnarco <omer.amasib@fnrco.com.sa>
2022-10-05 13:51:19 +0300

    add admin fetures

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False)
+    slug = models.SlugField(default="", null=False, blank=True)
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.slug)])
