1dd8679237c476d937815e9073c02dedda56f083
OmerFnarco <omer.amasib@fnrco.com.sa>
2022-10-12 13:03:02 +0300

    add author model and relationship with one to many

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,10 +4,15 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,null = True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, blank=True)
 
